c2a7aa7f75f62841e291702d0c37c4fbaca7eae9
frederico.hubler <frederico.hubler@ilegra.com>
2019-07-09 15:53:07 -0300

    Ajuste nas dependencias. Criada MainViewModel. Setado o bind para MainActivity. Adicionadas imagens iniciais

--- app/src/main/java/com/example/mastermemes/MainActivity.kt
+++ app/src/main/java/com/example/mastermemes/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.mastermemes
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mastermemes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.
     }
 }
